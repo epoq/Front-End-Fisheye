8a09fb5a5b381daa12afe33a90149e41bcb3a6c6
epoq <gasque974@gmail.com>
2022-05-17 13:01:09 +0200

    changes path

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -22,7 +22,7 @@ const photographerImg = document.createElement('img');
 const nameModale = document.getElementsByClassName('name_modale');
 
 
-fetch("https://cdn-subeeto.fra1.digitaloceanspaces.com/photographers.json")
+fetch("./photographers.json")
   .then((res) => res.json())
   .then((data) => {
 
